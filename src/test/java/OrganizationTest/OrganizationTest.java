ef888fa0893f8f0086c016f692bff8b4c7578fc4
Gokul <Gokul@LAPTOP-9B01OS6D>
2022-07-19 15:38:14 +0530

    my sample push to jen purpose

---
+++ src/test/java/OrganizationTest/OrganizationTest.java
@@ -0,0 +1,24 @@
+package OrganizationTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest 
+{
+	WebDriver driver;
+	@Test(groups="regression")
+	public void createOrg()
+	{
+		String value = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		if(browser=="chrome")
+			driver=new ChromeDriver();
+		else if(browser=="FireFox")
+			driver=new FirefoxDriver();
+		driver.get(value);
+		System.out.println("sucessfully created");
+		driver.close();
+	}
+}
---
+++ src/test/java/com/crm/espinaca/ContactTest/ContactTest.java
@@ -0,0 +1,30 @@
+package com.crm.espinaca.ContactTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ContactTest
+{
+	WebDriver driver ;
+	@Test(groups="smoke")
+	public void createContact()
+	{
+		driver=new FirefoxDriver();
+		//String browser = System.getProperty("Browser");
+		//System.out.println(browser);
+		//String url = System.getProperty("URL");
+		//System.out.println(url);		
+		System.out.println("contact created successfully");
+		driver.close();
+	}
+	@Test(groups="regression")
+	public void createContactandDelete()
+	{
+		driver=new ChromeDriver();
+		System.out.println("successfully deleted");
+		driver.close();
+	}
+}
+
